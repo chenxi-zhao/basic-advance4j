04b353c89f463e7175e6c99a32d48f19d50df846
chenxi.zhao <zhaochenxi1@jd.com>
2018-03-23 10:50:42 +0800

    从framework4j转移

---
+++ README.md
@@ -0,0 +1,1 @@
+basic-advance4j
---
+++ rpc-spring-netty/src/main/java/com/chenxi/rpc/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.chenxi.rpc.service;
+
+/**
+ * Created by chenxi on 2017/6/6.
+ *
+ * @author chenxi
+ */
+public interface HelloService {
+    String sayHello();
+}
